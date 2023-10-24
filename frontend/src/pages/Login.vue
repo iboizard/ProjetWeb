006e58fafb38741466846cd29f2eec3b38fac79b
i.boizard <i.boizard@lectra.com>
2023-10-24 12:14:09 +0200

    Design Login Page

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,55 +1,217 @@
 <template>
-    <div class="box">
-    
-     
-      <h1>Se connecter</h1>
-     
-      <div class="in">
-          <label for="name">Adresse Mail</label>
-        
-          <div>  <input type="text" v-model="username" placeholder="E-mail"
-               /><img v-if="username.length >= 4" alt=""></div>
-
+  <div class="container">
+    <div class="screen">
+      <div class="screen__content">
+        <form class="login">
+          <div class="login__field">
+            <i class="login__icon fas fa-user"></i>
+            <input type="text" class="login__input" placeholder="Email">
+          </div>
+          <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <input type="password" class="login__input" placeholder="Mot de passe">
+          </div>
+          <button class="button login__submit">
+            <span class="button__text">Se connecter</span>
+            <i class="button__icon fas fa-chevron-right"></i>
+          </button>
+        </form>
+        <div class="social-signup-div">
+          <a class="social-signup-href" href="/signup">S'inscrire</a>
+        </div>
       </div>
-    
-      <div class="in">
-      <label for="name">Mot de passe</label>
-      <div>
-          <input v-model="password" type="password" :name="string" placeholder="Entrer votre mot de passe" required/><img v-if="password.length >= 8" alt="">
+      <div class="screen__background">
+        <span class="screen__background__shape screen__background__shape4"></span>
+        <span class="screen__background__shape screen__background__shape3"></span>
+        <span class="screen__background__shape screen__background__shape2"></span>
+        <span class="screen__background__shape screen__background__shape1"></span>
       </div>
-    
-      </div>
-    
-    
-    
-      <button class="log">
-        Se connecter
-        </button>
-       
-    <br/>
-    <span>Pas encore de compte ? <a href="/signup">S'inscrire</a> </span> 
     </div>
+  </div>
+</template>
     
-    </template>
     
-    
-    <script>
-    export default {
-    
-      data() {
-        return {
-          password:"",
-          userLoggedIn: false,
-          username:"",    
-          isActive: false,
-        };
-        
-      },toggle() {
-    this.isActive = ! this.enable ;
-    },
+<script>
+export default {
+
+  data() {
+    return {
+      password: "",
+      userLoggedIn: false,
+      username: "",
+      isActive: false,
     };
-    
-    </script>
+
+  }, toggle() {
+    this.isActive = !this.enable;
+  },
+};
+
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Raleway, sans-serif;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.screen {
+  background: linear-gradient(90deg, #3e28e3, #08025f);
+  position: relative;
+  height: 600px;
+  width: 360px;
+  box-shadow: 0px 0px 24px #2f1fbe;
+}
+
+.screen__content {
+  z-index: 1;
+  position: relative;
+  height: 100%;
+}
+
+.screen__background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+  -webkit-clip-path: inset(0 0 0 0);
+  clip-path: inset(0 0 0 0);
+}
+
+.screen__background__shape {
+  transform: rotate(45deg);
+  position: absolute;
+}
+
+.screen__background__shape1 {
+  height: 520px;
+  width: 520px;
+  background: #FFF;
+  top: -50px;
+  right: 120px;
+  border-radius: 0 72px 0 0;
+}
+
+.screen__background__shape2 {
+  height: 220px;
+  width: 220px;
+  background: #036dde;
+  top: -172px;
+  right: 0;
+  border-radius: 32px;
+}
+
+.screen__background__shape3 {
+  height: 540px;
+  width: 190px;
+  background: linear-gradient(270deg, #2231a3, #1853d2);
+  top: -24px;
+  right: 0;
+  border-radius: 32px;
+}
+
+.screen__background__shape4 {
+  height: 400px;
+  width: 200px;
+  background: #2f27ba;
+  top: 420px;
+  right: 50px;
+  border-radius: 60px;
+}
+
+.login {
+  width: 320px;
+  padding: 30px;
+  padding-top: 156px;
+}
+
+.login__field {
+  padding: 20px 0px;
+  position: relative;
+}
+
+.login__icon {
+  position: absolute;
+  top: 30px;
+  color: #3128de;
+}
+
+.login__input {
+  border: none;
+  border-bottom: 2px solid #D1D1D4;
+  background: none;
+  padding: 10px;
+  padding-left: 24px;
+  font-weight: 700;
+  width: 75%;
+  transition: .2s;
+}
+
+.login__input:active,
+.login__input:focus,
+.login__input:hover {
+  outline: none;
+  border-bottom-color: #170de4;
+}
+
+.login__submit {
+  background: #fff;
+  font-size: 14px;
+  margin-top: 30px;
+  padding: 16px 20px;
+  border-radius: 26px;
+  border: 1px solid #D4D3E8;
+  text-transform: uppercase;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  color: #1d83ef;
+  box-shadow: 0px 2px 2px #379cd3;
+  cursor: pointer;
+  transition: .2s;
+}
+
+.login__submit:active,
+.login__submit:focus,
+.login__submit:hover {
+  border-color: #4138e9;
+  outline: none;
+}
+
+.button__icon {
+  font-size: 24px;
+  margin-left: auto;
+  color: #2b20f2;
+}
+.social-signup-div {	
+	position: absolute;
+	height: 140px;
+	width: 160px;
+	text-align: center;
+	bottom: 0px;
+	right: 0px;
+	color: #fff;
+}
+
+.social-signup-href {
+	padding: 20px 10px;
+	color: #fff;
+	text-decoration: none;	
+	text-shadow: 0px 0px 8px #7875B5;
+}
+</style>
     
     
     
